0484b3074a7f1a842443dd16b5da401f66bd9fea
Aman Kant <52783554+mnkant053305@users.noreply.github.com>
2020-07-28 16:17:57 +0530

    JS code to handle events for Switch On | Switch Off.
    JavaScript code to handle the switch on | switch off events when the user clicks on the Switch On | Switch Off button.

---
+++ Bulb_On_Off.js
@@ -0,0 +1,9 @@
+// event listener and anonymous function
+document.querySelector('#switch-on').addEventListener('click',function () {
+    document.querySelector('#bulb').src = 'Bulb_On.png'
+});
+
+// event listener and anonymous function
+document.querySelector('#switch-off').addEventListener('click',function () {
+    document.querySelector('#bulb').src = 'Bulb_Off.png'
+});
